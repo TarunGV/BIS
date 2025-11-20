651617d24d20a91d40b0a6fa9fc6087e272cad71
TarunGV <tarungowdav.cs23@bmsce.ac.in>
2025-11-20 12:52:29 +0530

    Update cuckoo.py

--- cuckoo.py
+++ cuckoo.py
@@ -59,3 +59,27 @@ def cuckoo_search(nests=10, max_iter=100, pa=0.25):
 best_solution, best_value = cuckoo_search(nests=10, max_iter=100, pa=0.25)
 print(f"Global minimum found at x = {best_solution}, f(x) = {best_value}")
 
+output
+Iteration 1, Best Fitness: 1.2921961942067006, Best Nest: -1.136748078602599
+Iteration 2, Best Fitness: 0.5765134389217328, Best Nest: -0.7592848206843943
+Iteration 3, Best Fitness: 0.5765134389217328, Best Nest: -0.7592848206843943
+Iteration 4, Best Fitness: 0.02266899319505372, Best Nest: 0.15056225687420377
+Iteration 5, Best Fitness: 0.0042965565622086685, Best Nest: 0.06554812401746268
+Iteration 6, Best Fitness: 0.0042965565622086685, Best Nest: 0.06554812401746268
+Iteration 7, Best Fitness: 0.0042965565622086685, Best Nest: 0.06554812401746268
+Iteration 8, Best Fitness: 0.0042965565622086685, Best Nest: 0.06554812401746268
+Iteration 9, Best Fitness: 0.002981228358757636, Best Nest: 0.05460062599236053
+Iteration 10, Best Fitness: 9.187111896745005e-06, Best Nest: -0.0030310248921354976
+Iteration 11, Best Fitness: 9.187111896745005e-06, Best Nest: -0.0030310248921354976
+Iteration 12, Best Fitness: 9.187111896745005e-06, Best Nest: -0.0030310248921354976
+Iteration 13, Best Fitness: 9.187111896745005e-06, Best Nest: -0.0030310248921354976
+Iteration 14, Best Fitness: 9.187111896745005e-06, Best Nest: -0.0030310248921354976
+Iteration 15, Best Fitness: 9.187111896745005e-06, Best Nest: -0.0030310248921354976
+Iteration 16, Best Fitness: 9.187111896745005e-06, Best Nest: -0.0030310248921354976
+Iteration 17, Best Fitness: 9.187111896745005e-06, Best Nest: -0.0030310248921354976
+Iteration 18, Best Fitness: 9.187111896745005e-06, Best Nest: -0.0030310248921354976
+Iteration 19, Best Fitness: 9.187111896745005e-06, Best Nest: -0.0030310248921354976
+Iteration 20, Best Fitness: 4.773953830111175e-07, Best Nest: 0.0006909380457111314
+
+
+
