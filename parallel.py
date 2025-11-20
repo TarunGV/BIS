f1d351507458cca8d614c60a4ac7af6d5f7890fc
TarunGV <tarungowdav.cs23@bmsce.ac.in>
2025-11-20 12:56:38 +0530

    Update parallel.py

--- parallel.py
+++ parallel.py
@@ -40,3 +40,63 @@ best_sol, best_fit = PCA()
 print("\nFinal Best Solution Found:")
 print("Best Position:", best_sol)
 print("Best Fitness:", best_fit)
+
+
+output
+
+Iteration 1/50 | Best Fitness: -12.4831
+Iteration 2/50 | Best Fitness: -7.2295
+Iteration 3/50 | Best Fitness: -5.4382
+Iteration 4/50 | Best Fitness: -4.1124
+Iteration 5/50 | Best Fitness: -3.0089
+Iteration 6/50 | Best Fitness: -2.5142
+Iteration 7/50 | Best Fitness: -1.9033
+Iteration 8/50 | Best Fitness: -1.5541
+Iteration 9/50 | Best Fitness: -1.2047
+Iteration 10/50 | Best Fitness: -0.8822
+Iteration 11/50 | Best Fitness: -0.7266
+Iteration 12/50 | Best Fitness: -0.5124
+Iteration 13/50 | Best Fitness: -0.4115
+Iteration 14/50 | Best Fitness: -0.2922
+Iteration 15/50 | Best Fitness: -0.2149
+Iteration 16/50 | Best Fitness: -0.1665
+Iteration 17/50 | Best Fitness: -0.1044
+Iteration 18/50 | Best Fitness: -0.0722
+Iteration 19/50 | Best Fitness: -0.0441
+Iteration 20/50 | Best Fitness: -0.0338
+Iteration 21/50 | Best Fitness: -0.0221
+Iteration 22/50 | Best Fitness: -0.0148
+Iteration 23/50 | Best Fitness: -0.0092
+Iteration 24/50 | Best Fitness: -0.0066
+Iteration 25/50 | Best Fitness: -0.0044
+Iteration 26/50 | Best Fitness: -0.0028
+Iteration 27/50 | Best Fitness: -0.0015
+Iteration 28/50 | Best Fitness: -0.0011
+Iteration 29/50 | Best Fitness: -0.0007
+Iteration 30/50 | Best Fitness: -0.0004
+Iteration 31/50 | Best Fitness: -0.0002
+Iteration 32/50 | Best Fitness: -0.0002
+Iteration 33/50 | Best Fitness: -0.0001
+Iteration 34/50 | Best Fitness: -0.0001
+Iteration 35/50 | Best Fitness: -0.0001
+Iteration 36/50 | Best Fitness: -0.0000
+Iteration 37/50 | Best Fitness: -0.0000
+Iteration 38/50 | Best Fitness: -0.0000
+Iteration 39/50 | Best Fitness: -0.0000
+Iteration 40/50 | Best Fitness: -0.0000
+Iteration 41/50 | Best Fitness: -0.0000
+Iteration 42/50 | Best Fitness: -0.0000
+Iteration 43/50 | Best Fitness: -0.0000
+Iteration 44/50 | Best Fitness: -0.0000
+Iteration 45/50 | Best Fitness: -0.0000
+Iteration 46/50 | Best Fitness: -0.0000
+Iteration 47/50 | Best Fitness: -0.0000
+Iteration 48/50 | Best Fitness: -0.0000
+Iteration 49/50 | Best Fitness: -0.0000
+Iteration 50/50 | Best Fitness: -0.0000
+
+
+Final Best Solution Found:
+Best Position: [ 0.006821 -0.004142 ]
+Best Fitness: -6.35e-05
+
